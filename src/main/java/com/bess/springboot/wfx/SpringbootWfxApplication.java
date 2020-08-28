98a3aea2704b51de829fb5de1f23deb6ea4fec7a
besscroft <631908942@qq.com>
2020-08-28 23:31:53 +0800

    初始化项目

---
+++ src/main/java/com/bess/springboot/wfx/SpringbootWfxApplication.java
@@ -0,0 +1,13 @@
+package com.bess.springboot.wfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWfxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWfxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bess/springboot/wfx/SpringbootWfxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bess.springboot.wfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWfxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
